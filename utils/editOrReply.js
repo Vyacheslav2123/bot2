3f5098a2f68ec14cd4924b36113a5ef9f4db366e
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2024-01-24 23:14:01 +0300

    Delete utils directory

--- utils/editOrReply.js
+++
@@ -1,6 +0,0 @@
-const { Context } = require("telegraf");
-
-Context.prototype.editOrReply = function (...args) {
-    if(!this.callbackQuery?.data || this.state.notEdit) return this.reply(...args);
-    else return this.editMessageText(...args);
-}
